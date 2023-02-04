0cd18f1b6c6ca6d35cbe7334f25273cc9db309da
gffice <114347535+gffice@users.noreply.github.com>
2023-02-05 00:46:58 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+mkdir ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod 777 ~/bin/repo
+mkdir -p ${HOME}/chromiumomos
+sudo mkdir -p /usr/local/path/to/source/chromiumos
+sudo ln -s /usr/local/path/to/source/chromiumos ${HOME}/chromiumos
+cd ~/chromiumos
+sudo ~/bin/repo init -u https://chromium.googlesource.com/chromiumos/manifest -g minilayout
+sudo ~/bin/repo sync
+export PATH=`pwd`/depot_tools:"$PATH"
+export BOARD=amd64-generic
+cros_sdk -- ./build_packages --board=${BOARD}
+cros_sdk -- ./build_image --board=${BOARD}
