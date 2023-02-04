cd774c897f93daec45ee7b6dd6e3b55a2961f5cc
gffice <114347535+gffice@users.noreply.github.com>
2023-02-05 01:25:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+cd ~/
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
@@ -5,10 +6,10 @@ chmod 777 ~/bin/repo
 mkdir -p ${HOME}/chromiumomos
 sudo mkdir -p /usr/local/path/to/source/chromiumos
 sudo ln -s /usr/local/path/to/source/chromiumos ${HOME}/chromiumos
+export PATH=`pwd`/depot_tools:"$PATH"
 cd ~/chromiumos
 sudo ~/bin/repo init -u https://chromium.googlesource.com/chromiumos/manifest -g minilayout
 sudo ~/bin/repo sync
-export PATH=`pwd`/depot_tools:"$PATH"
 export BOARD=amd64-generic
 cros_sdk -- ./build_packages --board=${BOARD}
 cros_sdk -- ./build_image --board=${BOARD}
